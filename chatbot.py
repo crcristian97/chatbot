50b18f098eec91a1b42136d6a00d7d9a3a0d04f5
Cristian Inclusion <crcristian97>
2023-03-14 10:57:27 -0300

    Chatbot

---
+++ chatbot.py
@@ -0,0 +1,23 @@
+import openai
+
+openai.api_key = "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"
+
+conversation = ""
+
+i = 1
+while (i != 0):
+    question = input("Humano: ")
+    conversation += "\nHumano: " + question + "\nAI:"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=conversation,
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=["\n", "Humano:" , "AI:"]
+    )
+    answer = response.choices[0].text.strip()
+    conversation += answer
+    print("AI: " + answer + "\n")
